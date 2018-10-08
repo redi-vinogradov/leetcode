6d1fe895e0302c7a0fd3aaa6fd224fc52e5c8aa4
Anton Vinogradov <anton.vinogradov@thomsonreuters.com>
2018-10-08 19:29:11 +0000

    adding plus_one

---
+++ plus_one.py
@@ -0,0 +1,15 @@
+def plusOne(digits):
+    """ First of all we need to convert original list to a string (done with 
+    map() and then convert it back to int to add 1 (so we can get 124 from
+    [1,2,3]). Next with list comprehension we are returning list of int values
+    converted from a string. 
+    """
+    s = int(''.join(map(str, digits))) + 1
+    print([int(s) for s in str(s)])
+
+def main():
+    digits = [1,2,3]
+    plusOne(digits)
+
+if __name__ == '__main__':
+    main()
